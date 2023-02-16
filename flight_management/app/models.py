dd788e08c3194cec3aaf6c14893e9e240ee2639b
Nati <netanelzelzermn@gmail.com>
2023-02-16 15:54:16 +0200

    Yes

--- flight_management/app/models.py
+++ flight_management/app/models.py
@@ -1,24 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# TODO: Add random activity generator facade to generate fake user activity:
+#       - User X just bought a flight ticket to Y in the popup on the bottom-right corner
+#       - User X just bought 
 
 # Create your models here.
-class Country(models.Model):
+class Airport(models.Model):
     name = models.CharField(max_length=50)
     code = models.CharField(max_length=3)
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+
 
 
 class AirlineCompany(models.Model):
     name    = models.CharField(max_length=50)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    # country = models.ForeignKey(Country, on_delete=models.CASCADE)
     user    = models.ForeignKey(User, on_delete=models.CASCADE)
     code    = models.CharField(max_length=3)
 
 
 class Flight(models.Model):
     airline_company     = models.ForeignKey(AirlineCompany, on_delete=models.CASCADE)
-    origin_country      = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='origin')
-    destination_country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='destination')
+    origin_airport      = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='origin')
+    destination_airport = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='destination')
     departure_time      = models.DateTimeField()
     landing_time        = models.DateTimeField()
     remaining_tickets   = models.IntegerField()
