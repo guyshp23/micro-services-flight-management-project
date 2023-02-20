02726dd11da1589b12e3db8399b55ea2df2438b0
Nati323 <121944905+Nati323@users.noreply.github.com>
2023-02-20 18:03:13 +0200

    Only inheriting, not abstract

--- Django-flights-management/app/Facades/BaseFacade.py
+++ Django-flights-management/app/Facades/BaseFacade.py
@@ -1,32 +1,31 @@
 from django.contrib.auth.models import User
-from abc import ABC, abstractmethod
 
 
-class BaseFacade(ABC):
+class BaseFacade():
     def __init__(self):
         pass
 
 
-    @abstractmethod
+
     def get_all_flights():
         pass
 
-    @abstractmethod
+
     def get_flight_by_id(flight_id: int):
         pass
 
-    @abstractmethod
+
     def get_flights_by_parameters(origin_airport_id: int, destination_airport_id: int, departing_date, returning_date):
         pass
 
-    @abstractmethod
+
     def get_all_airports():
         pass
 
-    @abstractmethod
+
     def create_new_user(user: User):
         pass
 
-    @abstractmethod
+
     def get_user_by_id(user: User):
         pass
