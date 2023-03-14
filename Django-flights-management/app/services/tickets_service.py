645b620cb37cfff3d979663de3dd458e0972a0ad
Nati <netanelzelzermn@gmail.com>
2023-03-14 18:19:36 +0200

    Django: Add book ticket functionality

--- Django-flights-management/app/services/tickets_service.py
+++ Django-flights-management/app/services/tickets_service.py
@@ -1,5 +1,46 @@
+import logging
+
+from app.exceptions.model_not_found import ModelNotFoundException
 from .base_service_interface import BaseServiceInterface
+from app.models import Ticket, Flight, Customer
 
 
 class TicketService(BaseServiceInterface):
-    pass
+    global logger
+    logger = logging.getLogger('main')
+    
+    def book_ticket(self, flight_id: int):
+        """
+        Creates a new ticket for the given flight (flight_id)
+        for the customer that sent the request and returns the created ticket.
+        
+        Params:
+            - flight_id: The flight ID.
+        
+        Raises:
+            - ModelNotFoundException: If the flight with the given ID doesn't exist.
+            - Exception: If a general error occurred in the flights service.
+
+        Returns:
+            - The ticket that was booked.
+        """
+        logger.debug("TicketService.book_ticket() called")
+        
+        # Check if the ticket exists by ID
+        flight = Flight.objects.filter(id=flight_id).first()
+
+        if flight == None:
+            logger.error('Flight not found, id: ' + str(flight_id))
+            raise ModelNotFoundException('Flight not found')
+        
+        # It's here because when creating a new ticket, it doens't
+        # Accept a customer ID, it accepts a customer object.
+        customer = Customer.objects.filter(id=1).first()
+
+        # Create a new ticket for the flight, customer = current user
+        # TODO: Get the current user ID by the token in the request
+        ticket = Ticket.objects.create(flight=flight, customer=customer)
+        ticket.save()
+
+        logger.debug('Booked a new ticket: ' + str(ticket))
+        return ticket
--- Django-flights-management/app/views/tickets.py
+++ Django-flights-management/app/views/tickets.py
@@ -1,11 +1,40 @@
 from rest_framework.views  import APIView, Response
+from app.exceptions.factory import ExceptionsFactory
+from app.exceptions.model_not_found import ModelNotFoundException
+from app.serializer.tickets_serializer import TicketsSerializer
+from app.services.tickets_service import TicketService
 
 
 class BookTicket(APIView):
-    pass
+    """
+    Book a ticket for a flight.
+
+    Params:
+        - flight_id: The flight ID.
+
+    Raises:
+        - Exception: If a general error occurred in the flights service.
+
+    Returns:
+        - The ticket that was booked.
+    """
+    serializer_class = TicketsSerializer
+
+    def post(self, request, flight_id: int):
+        try:
+            ticket     = TicketService.book_ticket(self, flight_id)
+            serializer = self.serializer_class(ticket)
+
+            # Return the ticket that was booked.
+            return Response(serializer.data, status=200)
+        except ModelNotFoundException as e:
+            # Return the error message.
+            return ExceptionsFactory.handle(e)
+
 
 class CancelTicket(APIView):
     pass
+
 
 class DeleteTicket(APIView):
     pass
